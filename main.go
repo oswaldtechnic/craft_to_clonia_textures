ce5421b80bbc0dda88fb09329659f094d383ab70
ostech <oswaldtechnic@zoho.com>
2024-08-23 03:40:16 -0800

    moved the unzip function into it's own file

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"archive/zip"
 	"errors"
 	"fmt"
 	imaging "github.com/disintegration/imaging"
@@ -74,7 +73,6 @@ func main() {
 		if !file.IsDir() &&
 			filepath.Ext(file.Name()) == ".zip" {
 			if _, err := os.Stat("input/" + FileNameWithoutExt(file.Name())); errors.Is(err, os.ErrNotExist) {
-				// path/to/whatever does not exist
 				fmt.Println("Unzipping:", file.Name())
 				Unzip("input/"+file.Name(), "input/"+FileNameWithoutExt(file.Name()))
 			} else {
@@ -249,68 +247,4 @@ func copyTextureBasic(src string, dest string) error {
 
 func FileNameWithoutExt(fileName string) string {
 	return strings.TrimSuffix(fileName, filepath.Ext(fileName))
-}
-
-func Unzip(src, dest string) error {
-	r, err := zip.OpenReader(src)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if err := r.Close(); err != nil {
-			panic(err)
-		}
-	}()
-
-	os.MkdirAll(dest, 0755)
-
-	// Closure to address file descriptors issue with all the deferred .Close() methods
-	extractAndWriteFile := func(f *zip.File) error {
-		rc, err := f.Open()
-		if err != nil {
-			return err
-		}
-		defer func() {
-			if err := rc.Close(); err != nil {
-				panic(err)
-			}
-		}()
-
-		path := filepath.Join(dest, f.Name)
-
-		// Check for ZipSlip (Directory traversal)
-		if !strings.HasPrefix(path, filepath.Clean(dest)+string(os.PathSeparator)) {
-			return fmt.Errorf("illegal file path: %s", path)
-		}
-
-		if f.FileInfo().IsDir() {
-			os.MkdirAll(path, f.Mode())
-		} else {
-			os.MkdirAll(filepath.Dir(path), f.Mode())
-			f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-			if err != nil {
-				return err
-			}
-			defer func() {
-				if err := f.Close(); err != nil {
-					panic(err)
-				}
-			}()
-
-			_, err = io.Copy(f, rc)
-			if err != nil {
-				return err
-			}
-		}
-		return nil
-	}
-
-	for _, f := range r.File {
-		err := extractAndWriteFile(f)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
 }
