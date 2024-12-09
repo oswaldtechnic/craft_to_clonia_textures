47b3812ef717b3bef8a6ee2c06fbeb0d7cd3d41b
ostech <oswaldtechnic@zoho.com>
2024-12-08 15:31:55 -0900

    normal vines added

--- fixes.go
+++ fixes.go
@@ -460,6 +460,38 @@ func single_chests_fix(inPath string, outPath string) *readWriteError {
 	}
 }
 
+func vine_fix(inPath string, outPath string) *readWriteError {
+	grayVine, err := imaging.Open(inPath + "vine.png")
+	if err != nil {
+		return &readWriteError{[]string{"block::vine.png failed to open!"}, "vine texture"}
+	}
+	_ = grayVine
+	dst := imaging.New(grayVine.Bounds().Dx(), grayVine.Bounds().Dy(), color.NRGBA{0, 0, 0, 0})
+	dst = imaging.Overlay(dst, grayVine, image.Point{0, 0}, 1.0)
+	dst = imaging.AdjustFunc(dst,
+		func(c color.NRGBA) color.NRGBA {
+			r := int(c.R) - 130
+			g := int(c.G) + 0
+			b := int(c.B) - 130
+			if r < 0 {
+				r = 0
+			}
+			if g > 255 {
+				g = 255
+			}
+			if b < 0 {
+				b = 0
+			}
+			return color.NRGBA{uint8(r), uint8(g), uint8(b), c.A}
+
+		})
+	if err = imaging.Save(dst, outPath+"mcl_core_vine.png"); err != nil {
+		return &readWriteError{[]string{"mcl_core_vine.png failed to save!"}, "vine texture"}
+	}
+
+	return nil
+}
+
 func water_fix(inPath string, outPath string) *readWriteError {
 	/*
 		craft water
@@ -472,50 +504,49 @@ func water_fix(inPath string, outPath string) *readWriteError {
 	wStill, err := imaging.Open(inPath + "water_still.png")
 	if err != nil {
 		return &readWriteError{[]string{"block::water_still.png failed to open!"}, "water textures"}
-	} else {
-		wStillX := wStill.Bounds().Dx()
-		wStillY := wStill.Bounds().Dy()
-		dst := imaging.New(wStillX, wStillY, color.NRGBA{0, 0, 0, 0})
-		dst = imaging.Overlay(dst, wStill, image.Point{0, 0}, 1.0)
-		plainWater := imaging.AdjustFunc(dst,
-			func(c color.NRGBA) color.NRGBA {
-				r := int(c.R) - 105
-				g := int(c.G) - 40
-				b := int(c.B) + 20
-				if r < 0 {
-					r = 0
-				}
-				if g < 0 {
-					g = 0
-				}
-				if b > 255 {
-					b = 255
-				}
-				return color.NRGBA{uint8(r), uint8(g), uint8(b), c.A}
-			})
-		if err = imaging.Save(plainWater, outPath+"default_water_source_animated.png"); err != nil {
-			return &readWriteError{[]string{"default_water_source_animated.png failed to save!"}, "water textures"}
-		}
+	}
+	wStillX := wStill.Bounds().Dx()
+	wStillY := wStill.Bounds().Dy()
+	dst := imaging.New(wStillX, wStillY, color.NRGBA{0, 0, 0, 0})
+	dst = imaging.Overlay(dst, wStill, image.Point{0, 0}, 1.0)
+	plainWater := imaging.AdjustFunc(dst,
+		func(c color.NRGBA) color.NRGBA {
+			r := int(c.R) - 105
+			g := int(c.G) - 40
+			b := int(c.B) + 20
+			if r < 0 {
+				r = 0
+			}
+			if g < 0 {
+				g = 0
+			}
+			if b > 255 {
+				b = 255
+			}
+			return color.NRGBA{uint8(r), uint8(g), uint8(b), c.A}
+		})
+	if err = imaging.Save(plainWater, outPath+"default_water_source_animated.png"); err != nil {
+		return &readWriteError{[]string{"default_water_source_animated.png failed to save!"}, "water textures"}
+	}
 
-		riverWater := imaging.AdjustFunc(dst,
-			func(c color.NRGBA) color.NRGBA {
-				r := int(c.R) - 105
-				g := int(c.G) - 0
-				b := int(c.B) + 45
-				if r < 0 {
-					r = 0
-				}
-				if g < 0 {
-					g = 0
-				}
-				if b > 255 {
-					b = 255
-				}
-				return color.NRGBA{uint8(r), uint8(g), uint8(b), c.A}
-			})
-		if err = imaging.Save(riverWater, outPath+"default_river_water_source_animated.png"); err != nil {
-			return &readWriteError{[]string{"default_river_water_source_animated.png failed to save!"}, "water textures"}
-		}
+	riverWater := imaging.AdjustFunc(dst,
+		func(c color.NRGBA) color.NRGBA {
+			r := int(c.R) - 105
+			g := int(c.G) - 0
+			b := int(c.B) + 45
+			if r < 0 {
+				r = 0
+			}
+			if g < 0 {
+				g = 0
+			}
+			if b > 255 {
+				b = 255
+			}
+			return color.NRGBA{uint8(r), uint8(g), uint8(b), c.A}
+		})
+	if err = imaging.Save(riverWater, outPath+"default_river_water_source_animated.png"); err != nil {
+		return &readWriteError{[]string{"default_river_water_source_animated.png failed to save!"}, "water textures"}
 	}
 
 	wFlowing, err := imaging.Open(inPath + "water_flow.png")
--- main.go
+++ main.go
@@ -225,6 +225,10 @@ func ConvertPack(inName string, outName string) {
 		failures += len(err.files)
 		textureErrorsLog += fmt.Sprint(err.Error() + "\n\n")
 	}
+	if err := vine_fix(texturePackLocation+craftPaths["block"], outPath+cloniaPaths["core"]); err != nil {
+		failures += len(err.files)
+		textureErrorsLog += fmt.Sprint(err.Error() + "\n\n")
+	}
 	if err := water_fix(texturePackLocation+craftPaths["block"], outPath+cloniaPaths["core"]); err != nil {
 		failures += len(err.files)
 		textureErrorsLog += fmt.Sprint(err.Error() + "\n\n")
